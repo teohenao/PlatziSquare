1aef2376d77b7c45fd3d4338763bed6a8efb47b1
teohenao <m-teo770@hotmail.com>
2019-08-15 23:17:15 -0500

    leccion Directivas ngClass ngStyles pratzi

--- platzisquare/src/app/app.component.ts
+++ platzisquare/src/app/app.component.ts
@@ -8,15 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'platzisquare';
   //variables para trabajar con map
-  lat:number=4.6455897;
-  lng:number=-75.6017297;
+  lat:number=4.5709817;
+  lng:number=-75.6557092;
 
   //arreglo para recorrer por la directiva *ngFor
   lugares:any = [
     //variable active es para practica con la directiva *ngIf
-    {active: true,nombre:'floreria'},
+    { active: true,nombre:'floreria'},
     { active: false,nombre:'veterinaria'},
-    { active: true,nombre:'casita'}
+    { active: true,nombre:'casita'},
+    { active: true,nombre:'cariceria'},
+    { active: false,nombre:'academica'},
+    { active: true,nombre:'panaderia'}
   ];
 
   constructor(){
