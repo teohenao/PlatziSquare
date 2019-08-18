73724b840a97ad46c889aa12f60b0c38efbdff6e
teohenao <m-teo770@hotmail.com>
2019-08-18 10:06:33 -0500

    leccion Ruteo parametros tipo Query platzi

--- platzisquare/src/app/detalle/detalle.component.ts
+++ platzisquare/src/app/detalle/detalle.component.ts
@@ -10,6 +10,8 @@ export class DetalleComponent {
     constructor(private route: ActivatedRoute){
         //console para ver que id nos esta llegando como parametro al detalle
         console.log('id : ' + this.route.snapshot.params['id']);
+        //console para ver el objeto que tiene esos parametros que enviamos, si aparece indefinido mirar si esta snapchot
+        console.log(this.route.snapshot.queryParams['action']);
     }
   
 }
